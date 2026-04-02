916ca490aaa9d262f45e18fdd99446fa34d7c3bb
dheim123 <dheim@midland.k12.ia.us>
2026-04-02 11:49:11 -0500

    Refactor ListUtility class formatting

--- ListUtility.cs
+++ ListUtility.cs
@@ -1,11 +1,11 @@
-﻿namespace knightmoves;
+namespace knightmoves;
 
-public class ListUtility{
+public class ListUtility {
 
-    public List<string> Copy(params string[] items){
-        List<string> listItems = new List<string>();
+    public List<string> Copy(params string[] items) {
+        var listItems = new List<string>();
 
-        foreach(string item in items){
+        foreach(string item in items) {
             listItems.Add(item);
         }
         
